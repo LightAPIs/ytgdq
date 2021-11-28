2663a6b4fd3f8dbb49782a821ef93b1af4ce95e0
Light <47911535+LightAPIs@users.noreply.github.com>
2021-11-28 14:30:06 +0800

    chore(release): 1.1.0
    更新成绩版本至 s04

--- WindowsFormsApplication2/Glob.cs
+++ WindowsFormsApplication2/Glob.cs
@@ -19,13 +19,13 @@ namespace WindowsFormsApplication2
     /// </summary>
     public class Glob
     {
-        public static string Ver = "1.0.0";
+        public static string Ver = "1.1.0";
 
         public static string Form = "雨天跟打器v" + Ver;
         /// <summary>
         /// 成绩版本
         /// </summary>
-        public static string Instration = "s03";
+        public static string Instration = "s04";
 
         /// <summary>
         /// 显示实时数据

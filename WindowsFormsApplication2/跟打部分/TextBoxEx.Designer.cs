d94f3f8e9813a6c22cd7d0cf89b833e82e280088
Light <47911535+LightAPIs@users.noreply.github.com>
2021-12-15 22:57:43 +0800

    refactor(TextBoxEx): 移除多余的代码

--- WindowsFormsApplication2/跟打部分/TextBoxEx.Designer.cs
+++ WindowsFormsApplication2/跟打部分/TextBoxEx.Designer.cs
@@ -28,21 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 0;
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
